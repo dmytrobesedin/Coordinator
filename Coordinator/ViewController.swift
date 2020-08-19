1ec64f090c16a50907916d64576b090d893bbdae
dmytrobesedin <dbesedin7@gmail.com>
2020-08-19 15:39:11 +0300

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by Дмитрий Беседин on 19.08.2020.
+//  Copyright © 2020 DmytroBesedin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
